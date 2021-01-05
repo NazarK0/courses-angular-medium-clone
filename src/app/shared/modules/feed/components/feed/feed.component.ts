220e6adbd715b2ba8c45217256fc2019d0a8ec63
Nazar Vanivskyi <staticman999@gmail.com>
2021-01-05 16:39:43 +0200

    chapter 28 pagination

--- src/app/shared/modules/feed/components/feed/feed.component.ts
+++ src/app/shared/modules/feed/components/feed/feed.component.ts
@@ -1,10 +1,12 @@
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { isLoadingSelector, errorSelector, feedSelector } from './../../store/selectors';
 import { GetFeedResponseInterface } from './../../types/getFeedResponse.interface';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
 import { getFeedAction } from './../../store/actions/getFeed.action';
 import { Store, select } from '@ngrx/store';
 import { Input, OnInit } from '@angular/core';
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   // tslint:disable-next-line: component-selector
@@ -12,28 +14,50 @@ import { Component } from '@angular/core';
   templateUrl: './feed.component.html',
   styleUrls: ['./feed.component.scss'],
 })
-export class FeedComponent implements OnInit {
+export class FeedComponent implements OnInit, OnDestroy {
   // tslint:disable-next-line: no-input-rename
   @Input('apiUrl') apiUrlProps: string;
 
   isLoading$: Observable<boolean> = new Observable();
   error$: Observable<string | null> = new Observable();
   feed$: Observable<GetFeedResponseInterface | null> = new Observable();
+  limit: number = environment.limit;
+  baseUrl = '/';
+  queryParamsSubscription: Subscription = new Subscription();
+  currentPage = 1;
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute,
+  ) {}
 
   initializeValues(): void {
     this.isLoading$ = this.store.pipe(select(isLoadingSelector));
     this.error$ = this.store.pipe(select(errorSelector));
     this.feed$ = this.store.pipe(select(feedSelector));
+    this.baseUrl = this.router.url.split('?')[0];
   }
 
   fetchData(): void {
     this.store.dispatch(getFeedAction({ url: this.apiUrlProps }));
   }
 
+  initializeListeners(): void {
+    this.queryParamsSubscription = this.route
+      .queryParams
+      .subscribe((params: Params) => {
+        this.currentPage = Number(params.page || '1') ;
+      });
+  }
+
   ngOnInit(): void {
     this.initializeValues();
     this.fetchData();
+    this.initializeListeners();
+  }
+
+  ngOnDestroy(): void {
+    this.queryParamsSubscription.unsubscribe();
   }
 }
